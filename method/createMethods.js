69b286629e7788fe3340a80131b827d371376260
Jan Küster <jkuester@uni-bremen.de>
2019-10-10 23:06:11 +0200

    createMethods fixed missing method return

--- method/createMethods.js
+++ method/createMethods.js
@@ -2,22 +2,20 @@ import { check, Match } from 'meteor/check'
 import { isObject, maybe } from '../utils'
 import ValidatedMethod from './ValidatedMethod'
 
-export const getCreateMethod = schemaResolver => {
-  const createMethod = ({ name, schema, run, roles, group, isPublic }) => {
-    check(name, String)
-    check(schema, isObject)
-    check(run, Function)
-    check(isPublic, maybe(Boolean))
-    check(roles, isPublic ? maybe([ String ]) : [ String ])
-    check(group, isPublic ? maybe(String) : String)
+export const getCreateMethod = (schemaResolver, validate = true, useRoles = true)=> ({ name, schema, run, roles, group, isPublic }) => {
+  check(name, String)
+  check(schema, isObject)
+  check(run, Function)
+  check(isPublic, maybe(Boolean))
+  check(roles, isPublic ? maybe([ String ]) : [ String ])
+  check(group, isPublic ? maybe(String) : String)
 
-    const validationSchema = schemaResolver(schema)
-    const validate = function validate (document = {}) {
-      validationSchema.validate(document)
-    }
-
-    return new ValidatedMethod({ name, validate, run, roles, group, isPublic })
+  const validationSchema = schemaResolver(schema)
+  const validate = function validate (document = {}) {
+    validationSchema.validate(document)
   }
+
+  return new ValidatedMethod({ name, validate, run, roles, group, isPublic })
 }
 
 export const getCreateMethods = schemaResolver => {
