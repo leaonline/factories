f364cda1ac44931924c571790b156b6ae7833610
Jan Küster <jkuester@uni-bremen.de>
2019-10-10 23:06:29 +0200

    rateLimit accounts with defaults

--- ratelimit/rateLimit.js
+++ ratelimit/rateLimit.js
@@ -1,9 +1,10 @@
 import { RateLimiterRegistry } from './RateLimiterRegistry'
 import { check } from 'meteor/check'
 import { isObject } from '../utils'
+import { Accounts } from 'meteor/accounts-base'
 
 export const rateLimitMethods = methods => {
-  check(methods, [isObject])
+  check(methods, [ isObject ])
   return methods.map(rateLimitMethod)
 }
 
@@ -13,11 +14,63 @@ const rateLimitMethod = ({ name, userId, connectionId, clientAddress, numRequest
 }
 
 export const rateLimitPublications = publications => {
-  check(publications, [isObject])
+  check(publications, [ isObject ])
   return publications.map(rateLimitPublication)
 }
 
 const rateLimitPublication = ({ name, userId, connectionId, clientAddress, numRequests, timeInterval }) => {
   // no need to check since RateLimiterRegistry does that already
   return RateLimiterRegistry.addPublication({ name, userId, connectionId, clientAddress, numRequests, timeInterval })
+}
+
+export const rateLimitAccounts = ({
+                                    numRequestsMethods = 5,
+                                    timIntervalMethods = 10000,
+                                    numRequestsPublications = 20,
+                                    timeIntervalPublications = 10000,
+                                    accountMethods = [
+                                      'login',
+                                      'loginWithToken',
+                                      'logout',
+                                      'logoutOtherClients',
+                                      'getNewToken',
+                                      'removeOtherTokens',
+                                      'configureLoginService',
+                                      'changePassword',
+                                      'forgotPassword',
+                                      'resetPassword',
+                                      'verifyEmail',
+                                      'createUser',
+                                      'ATRemoveService',
+                                      'ATCreateUserServer',
+                                      'ATResendVerificationEmail'
+                                    ],
+                                    accountPublications = [
+                                      'meteor.loginServiceConfiguration',
+                                      'meteor_autoupdate_clientVersions',
+                                      '_roles'
+                                    ]
+                                  } = {}) => {
+  Accounts.removeDefaultRateLimit()
+
+  accountMethods.forEach(name => RateLimiterRegistry.addMethod({
+    name,
+    numRequests: numRequestsMethods,
+    timeInterval: timIntervalMethods
+  }))
+
+  accountPublications.forEach(name => RateLimiterRegistry.addPublication({
+    name,
+    timeInterval: timeIntervalPublications,
+    numRequests: numRequestsPublications
+  }))
+}
+
+export const runRateLimiter = (limitExceededCallback, sanityCheck = true) => {
+  if (sanityCheck) {
+    RateLimiterRegistry.sanityCheck(true)
+  }
+  Meteor.startup(() => {
+    RateLimiterRegistry.run(limitExceededCallback)
+  })
 }
