7265ab9fd5363e1a74264fd032fe2f2db20431bf
Jan Kuester <jkuester@uni-bremen.de>
2019-10-28 15:22:38 +0100

    1.1.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'leaonline:factories',
-  version: '1.0.1',
+  version: '1.1.0',
   // Brief, one-line summary of the package.
   summary: 'Provides an easy way to create Meteor Collections, Methods, Publications using factory methods.',
   // URL to the Git repository containing the source code for this package.
@@ -8,17 +8,18 @@ Package.describe({
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
-});
+})
 
-Package.onUse(function(api) {
-  api.versionsFrom('1.8.1');
-  api.use('ecmascript');
-  api.mainModule('factories.js');
-});
+Package.onUse(function (api) {
+  api.versionsFrom('1.8.1')
+  api.use('ecmascript')
+  api.use('leaonline:webapp')
+  api.mainModule('factories.js')
+})
 
-Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('leaonline:factories');
-  api.mainModule('factories-tests.js');
-});
+Package.onTest(function (api) {
+  api.use('ecmascript')
+  api.use('tinytest')
+  api.use('leaonline:factories')
+  api.mainModule('factories-tests.js')
+})
